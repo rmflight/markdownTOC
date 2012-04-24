907d236385b7b34a43992f40e6c9f9ff73801d21
Robert M Flight <r.flight79@gmail.com>
2012-04-24 16:25:21 -0400

    Allows new TOC on file with an existing TOC

--- markdownTOC.r
+++ markdownTOC.r
@@ -24,26 +24,33 @@ mdInpageTOC <- function(mdFile, maxLevel=Inf, tocString="TOC"){
 	lowerHead <- headerLoc > tocLoc
 	headerLoc <- headerLoc[lowerHead]
 	headerLev <- headerLev[lowerHead]
-		
+	
+	# find anything that was already set
+	headerAnch <- regexpr('<a', mdText)
+	headerAnch <- headerAnch[headerLoc]
+	nullAnch <- which(headerAnch == -1)
+	headerAnch[nullAnch] <- nchar(mdText[nullAnch])
+	
 	# loop through the header lines, getting all the data we need
 	tocText <- sapply(seq(1, length(headerLoc)), function(hI){
 		tmpTxt <- mdText[headerLoc[hI]]
-		headTxt <- substring(tmpTxt, headerLev[hI]+2) # assumes there is one space between the # and the string
-		anchTxt <- tolower(headTxt)
+		headTxt <- substring(tmpTxt, 1, headerAnch[hI] - 1)
+		headName <- substring(headTxt, headerLev[hI]+2) # assumes there is one space between the # and the string
+		anchTxt <- tolower(headName)
 		
 		# remove any blanks
 		anchTxt <- gsub(" ", "", anchTxt)
 		
-		mdText[headerLoc[hI]] <<- paste(mdText[headerLoc[hI]], '<a id="', anchTxt, '"></a>', sep="")
+		mdText[headerLoc[hI]] <<- paste(headTxt, '<a id="', anchTxt, '"></a>', sep="")
 		nBlank <- paste(rep(" ", (headerLev[hI] - 1) * 4 ), collapse="")
-		headTxt <- paste(nBlank, "* [", headTxt, "](#", anchTxt, ")", sep="")
-		return(headTxt)
+		headName <- paste(nBlank, "* [", headName, "](#", anchTxt, ")", sep="")
+		return(headName)
 		
 	})
 	
 	# and insert the TOC immediately after the tocLoc
 	topTxt <- mdText[1:tocLoc]
-	botTxt <- mdText[seq(tocLoc+1,length(mdText))]
+	botTxt <- mdText[seq(headerLoc[1],length(mdText))] # this removes any current TOC in the document
 	mdText <- c(topTxt,"", tocText, "", botTxt)
 	cat(mdText, file=mdFile, sep="\n")
 	
