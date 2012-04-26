e88cdb910099c43d3f77ebc08ca02bc3b895da52
Robert M Flight <r.flight79@gmail.com>
2012-04-26 09:58:10 -0400

    create a "Main" table of contents
    Now is able to take a series of input files, and for each one extract the existing TOC, and create a top level index into them.

--- markdownTOC.r
+++ markdownTOC.r
@@ -63,6 +63,69 @@ mdInpageTOC <- function(mdFile, maxLevel=Inf, tocString="TOC"){
 	
 }
 
+# grabs the table of contents from a given file
+grabToc <- function(mdText, tocString="TOC"){
+	allHead <- regexpr("^[#]{1} ([[:alnum:]]| )", mdText)
+	mainHead <- (which(allHead != -1))[1]
+	fileHead <- substring(mdText[mainHead], 3)
+	tocHead <- regexpr(paste('^[#]+ ', tocString, sep=""), mdText)
+	tocLines <- regexpr("\\[([[:alnum:]]| )+\\]\\(#([[:alnum:]]| )+\\)", mdText) # looking for [textstring](#anchorstring)
+	
+	tocLines <- which(tocLines != -1)
+	tocLines <- tocLines[tocLines > which(tocHead != -1)]
+	tocTxt <- mdText[tocLines]
+	return(list(fileHead=fileHead, tocTxt=tocTxt))
+}
+
+
+# OK, you have a bunch of markdown files, and now you want to generate a navigation page that provides links to each one, and if
+# a table of contents is present in the file, to make that available as well.
+# this means you need to provide to this function the main file (or blank if you just want the text output), the list of filenames
+# you want to be linked to, what string identifies the table of contents in those files, and the directory path to prepend to each link.
+# For example, for Github wiki pages, the relative path from "github.com" should be supplied, /user/project/wiki/
+
+# assumptions: The link text to use is the first header in each subFile, the TOC in each subFile is between '# tocString' and the next header, and all files are in the same directory.
+mdFullToc <- function(mainFile=NULL, subFiles, tocString="TOC", filePath=NULL){
+	subSplit <- strsplit(subFiles, .Platform$file.sep)
+	subSplit <- sapply(subSplit, function(x){x[[length(x)]]})
+	
+	# now for each file
+	mainToc <- sapply(subFiles, function(sF){
+		mdText <- scan(sF, what="character", sep="\n", blank.lines.skip=F)
+		sFWiki <- strsplit(sF, "\\.")[[1]][1]
+		wikiLink <- file.path(filePath, sFWiki, fsep="/") # creating the full path the file on the server
+		
+		tocDat <- grabToc(mdText, tocString)
+		headTOC <- paste('* [', tocDat$fileHead, '](', wikiLink, ')', sep="")
+		subLinkLoc <- regexpr('\\(#', tocDat$tocTxt)
+		subLinkTxt <- sapply(seq(1,length(subLinkLoc)), function(x){
+			tmpTxt <- tocDat$tocTxt[x]
+			str1 <- substring(tmpTxt, 1, subLinkLoc[x])
+			str2 <- substring(tmpTxt, subLinkLoc[x]+1)
+			paste(paste(rep(" ", 4), sep="", collapse=""), str1, wikiLink, str2, sep="")
+		})
+		return(c(headTOC, subLinkTxt))
+	})
+	if (is.null(mainFile)){
+		return(unlist(mainToc, use.names=F))
+	} else {
+		mainTxt <- scan(mainFile, what="character", sep="\n", blank.lines.skip=F)
+		allHead <- which(regexpr("^[#]{1} ([[:alnum:]]| )", mainTxt) != -1)
+		tocHead <- which(regexpr(paste('^[#]+ ', tocString, sep=""), mainTxt) != -1)
+		
+		allHead <- allHead[allHead > tocHead]
+		
+		topTxt <- mainTxt[1:tocHead]
+		botTxt <- mainTxt[seq(allHead[1],length(mainTxt))] # this removes any current TOC in the document
+		mainTxt <- c(topTxt,"", unlist(mainToc, use.names=F), "", botTxt)
+		cat(mainTxt, file=mainFile, sep="\n")
+		
+	}
+}
+
+# subFiles <- c("Programming-Resources.md", "Journals-I-follow.md")
+# tocString <- "TOC"
+# filePath <- "rmflight/general/wiki"
 # mdFile <- "Programming-Resources.md"
 # maxLevel <- Inf
 # tocString <- "TOC"
